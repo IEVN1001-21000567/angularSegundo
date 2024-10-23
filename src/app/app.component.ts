5cd354c88541af580d941ab74fbaf4d745d198ba
Alicia Elizabeth Falcon Medina <afm75581@gmail.com>
2024-10-22 20:55:00 -0600

    Martes 22 Oct ServiciosFinal

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { TemapComponent } from './tem/temap/temap.component';
+import { AddMessageComponent } from './tem/add-message/add-message.component';
+import { ListMessageComponent } from './tem/list-message/list-message.component';
+//import { TemapComponent } from './tem/temap/temap.component';
 //import { Ejemplo1Component } from './formulario/ejemplo1/ejemplo1.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [TemapComponent],
+  imports: [AddMessageComponent, ListMessageComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
